f1e8ab3cce10995a53ba931c10e249b1eea0dbc9
trustworthy-request-7186 <avdemaria@gmail.com>
2019-05-13 19:35:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
-  return playist;
+  return playlist;
 }
 
 function removeFromPlayist(playist, artistName){
