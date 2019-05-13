8c77f84a5b8c522d76abcf7cd9f4a5e6de94349b
trustworthy-request-7186 <avdemaria@gmail.com>
2019-05-13 19:32:48 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playist.artistName = songTitle;
+  return playist;
+}
+
+function removeFromPlayist(playist, artistName){
+  delete playlist.artistName;
+  return playlist;
+}
